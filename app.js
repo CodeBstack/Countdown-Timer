7879b2c5865e6aafe8e299fff3f486a4bf1b4771
Samuel <ssamuelolumide@gmail.com>
2022-05-25 17:33:50 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-
+//Selection of modal elements
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
@@ -32,27 +32,48 @@ document.addEventListener('keydown', function (e) {
     }
 });
 
-//which gets the Date
-const launchDate = () => {
-    const date = new Date('may 25, 2022 00:00:00').getTime();
-    // console.log(date);
-    return date;
-}
+// //which gets the Date - hardcoded
+// const launchDate = () => {
+//     const date = new Date('may 25, 2022 00:00:00').getTime();
+//Using this Date format -- console.log(new Date(2037, 10, 31)) //Tue Dec 01 2037 00:00:00 GMT+0100 (West Africa Standard Time)
+
+//     // console.log(date);
+//     return date;
+// }
+
+//Selection of the input elements
+const year = Number(document.getElementById('year-select').value);
+const month = Number(document.getElementById('month-select').value);
+const day = Number(document.getElementById('day-select').value);
+// const eventName = document.getElementById('event-name').textContent;
+
+console.log(year, month, day)
 
 
 
-//selection of elements
-const countdownApp = document.querySelector('.countdown');
-// const message = document.querySelector('.message');
-const title = document.querySelector('h2');
-const timers = document.querySelector('.timers')
 
-const dateFormat = (t) => {
-    return t < 10 ? '0' + t : t;
-}
 
-const render = (time) => {
-    countdownApp.innerHTML = `
+// const launchDate = (year, month, day) => {
+//     const date = new Date(year, month, day).getTime();
+//     console.log(year, month, day, date);
+//     return date;
+// }
+
+
+//function which does the countdown and also renders the html
+const countDownFunc = function () {
+    //selection of elements
+    const countdownApp = document.querySelector('.countdown');
+
+    const title = document.querySelector('h2');
+    const timers = document.querySelector('.timers')
+
+    const dateFormat = (t) => {
+        return t < 10 ? '0' + t : t;
+    }
+
+    const render = (time) => {
+        countdownApp.innerHTML = `
     <div class="comp-container">
     <div class="day components">
       <p class="timers">${dateFormat(time.days)}</p>
@@ -88,32 +109,44 @@ const render = (time) => {
           <p>seconds</p>
         </div>
       </div> `
+    }
+
+    const displayMessage = () => {
+        title.innerHTML = 'EXPIRED!!!'
+        // timers.innerHTML = '';
+        // title.style.display = 'none';
+    }
+
+    const hideMessage = () => {
+        title.innerHTML = 'THANK YOU 🙂🙂 '
+    }
+
+    const complete = () => {
+        displayMessage();
+
+        setTimeout(() => {
+            hideMessage();
+            countdownTimer.setExpiredDate(launchDate(year, month, day));
+        }, 1000 * 30);
+    }
+
+    const countdownTimer = new CountDown(
+        launchDate(year, month, day),
+        render,
+        complete
+    )
 }
+// countDownFunc();
 
-const displayMessage = () => {
-    title.innerHTML = 'EXPIRED!!!'
-    // timers.innerHTML = '';
-    // title.style.display = 'none';
-}
-// const reminder = ()=>{
-//     if time.day
-// }
 
-const hideMessage = () => {
-    title.innerHTML = 'THANK YOU 🙂🙂 '
-}
+//Event listener added to the Enter button on the form.
+document.querySelector('.btn').addEventListener('click', function (e) {
+    e.preventDefault();
 
-const complete = () => {
-    displayMessage();
 
-    setTimeout(() => {
-        hideMessage();
-        countdownTimer.setExpiredDate(launchDate());
-    }, 1000 * 30);
-}
 
-const countdownTimer = new CountDown(
-    launchDate(),
-    render,
-    complete
-)
+    closeModal();
+
+
+    // countDownFunc();
+});
